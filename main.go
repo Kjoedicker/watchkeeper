674308f62458608eb5edd91b93ce4004b5230171
kjoedicker <kjoedicker@gmail.com>
2023-09-30 09:24:35 -0400

    feat: add better CLI argument support and implement a `WatchLoop` for running connection checks on an interval

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 	"os"
@@ -42,28 +43,47 @@ func displayPorts(status string, ports []string) {
 	}
 }
 
-func parseCommandLineArguments() (host string) {
-	if len(os.Args) == 1 {
+var (
+	DEFAULT_STRING = ""
+	DEFAULT_INT    = 0
+)
+
+func parseCommandLineArguments() (host string, interval int) {
+	hostPtr := flag.String("host", DEFAULT_STRING, "Hostname Example: github.com | 140.82.113.4")
+	intervalPtr := flag.Int("interval", DEFAULT_INT, "Interval to check connection")
+
+	flag.Parse()
+
+	if *hostPtr == DEFAULT_STRING {
 		fmt.Println("A host must be provided")
 		os.Exit(1)
 	}
-	return os.Args[1]
+
+	return *hostPtr, *intervalPtr
 }
 
 func main() {
-	host := parseCommandLineArguments()
+	host, interval := parseCommandLineArguments()
 
-	var openPorts []string
-	var closedPorts []string
+WatchLoop:
+	for {
+		var openPorts []string
+		var closedPorts []string
 
-	var wg sync.WaitGroup
-	for _, port := range ports {
-		wg.Add(1)
-		go checkTCPPort(&wg, host, port, &openPorts, &closedPorts)
+		var wg sync.WaitGroup
+		for _, port := range ports {
+			wg.Add(1)
+			go checkTCPPort(&wg, host, port, &openPorts, &closedPorts)
+		}
+		wg.Wait()
+
+		fmt.Println(host + " Ports:")
+		displayPorts("open", openPorts)
+		displayPorts("closed", closedPorts)
+
+		if interval == 0 {
+			break WatchLoop
+		}
+		time.Sleep(time.Duration(interval) * time.Second)
 	}
-	wg.Wait()
-
-	fmt.Println(host + " Ports:")
-	displayPorts("open", openPorts)
-	displayPorts("closed", closedPorts)
 }
